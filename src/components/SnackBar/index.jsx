703158b8f44a83c18ba23ced6f4951906e0030a8
Rizsid <rizsid16gmail.com>
2024-08-29 21:38:14 +0500

    add redux

--- src/components/SnackBar/index.jsx
+++ src/components/SnackBar/index.jsx
@@ -5,7 +5,7 @@ import { useSnackBarManager } from '../../hooks/useSnackBarManager';
 const SnackBar = () => {
     const { isShowSnackBar, isError } = useSnackBarManager();
     return (
-        <div className={`snackBar ${isShowSnackBar ? 'show' : 'hide'} ${isError ? 'error' : ''}`}>
+        <div className={`snackBar ${isShowSnackBar && 'show'} ${isError ? 'error' : ''}`}>
             <i className={`icon ${isError ? "ri-error-warning-fill" : "ri-check-double-line"}`}></i>
             <span className="message">
                 {isShowSnackBar}
