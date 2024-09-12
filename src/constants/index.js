c8a76f0f71fbb1ea3dd800a0d13a2272a95eab68
Rizsid <rizsid16gmail.com>
2024-09-12 08:16:06 +0500

    changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,8 +3,8 @@ export const Config = {
     userToken : 'user-token',
     imgUrl : 'http://localhost:3000/public/',
 
-    fnGetPaginationUrl: function(apiUrl, params) {
-        let url = `${apiUrl}?`;
+    fnGetPaginationUrl: function(apiEndPoint, params) {
+        let url = `${apiEndPoint}?`;
         if (params?.search) { url += `search=${params.search}&`}
         if (params?.page) { url += `page=${params.page}&`}
         if (params?.limit) { url += `limit=${params.limit}`}
